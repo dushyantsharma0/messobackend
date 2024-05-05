4d5c36624acac8a8864519cb06580a161c6ee2a5
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-05-05 12:06:04 +0530

    ad

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,37 +5,37 @@ const cartDetail = require('../models/cartDetail')
 const countModel = require('../models/countModel')
 
 
-const otpGenerator = require('otp-generator'); // Import the otpGenerator library
+const Register = async (req,resp)=>{
+   const otp= otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false,lowerCaseAlphabets:false });
+     const {email}=req.body
+     const user=await RegisterSchma.findOne({email})
 
-const Register = async (req, resp) => {
-    const { email } = req.body;
-    const user = await RegisterSchma.findOne({ email });
-
-    if (user) {
-        const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false }); // Generate OTP
-        const msg = `<p style="color:#313030">Your OTP for Meesho Login is <span style="font-weight:800">${otp}</span> and is Valid for 30 Mins. Please DO NOT Share this OTP with anyone to Keep Your Account safe <span style="font-weight:700">${user._id}</span> Meesho</p> <br />  <div style="display:flex; justify-content: center" ><img style="width:100wh" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7H4O7Nve77imJCVuOH3uu-OBDtYG_uK2R2g&s" alt="" /></div>`;
-        user.otp = otp;
-
-        await user.save();
-        mailer.sendMail(email, 'BX-MSHOTP', msg);
-        return resp.status(400).json({
-            message: user
-        });
-    } else {
-        const otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false }); // Generate OTP
-        const newUser = new RegisterSchma({
+     if(user){
+        const msg = `<p style={color:#313030}>Your OTP for Meesho Login is <span style="font-weight:800">${otp}</span> and is Valid for 30 Mins. Please DO NOT Share this OTP with anyone to Keep Your Account safe <span style="font-weight:700">${user._id}</span> Meesho</p> <br />  <div style={display:flex,justify-content: center} ><img style={width:100wh} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7H4O7Nve77imJCVuOH3uu-OBDtYG_uK2R2g&s" alt="" /></div>        `;
+        user.otp=otp
+         
+         await user.save()
+         mailer.sendMail(email,'BX-MSHOTP',msg)
+         return resp.status(400).json({
+             message:user
+         })
+     }else{
+         const newUser=new RegisterSchma({
             email,
             otp
-        });
-
-        const msg = `<h1>Your OTP for Meesho Login is ${otp} and is Valid for 30 Mins. Please DO NOT Share this OTP with anyone to Keep Your Account safe ${newUser._id} Meesho </h1>`;
-
-        mailer.sendMail(email, 'BX-MSHOTP', msg);
-        await newUser.save();
-        return resp.status(200).json({
-            message: newUser
-        });
-    }
+         })
+         
+         const msg=`<h1>Your OTP for Meesho Login is ${otp} and is Valid for 30 Mins. Please DO NOT Share this OTP with anyone to Keep Your Account safe ${newUser._id} Meesho </h1>`
+        
+         mailer.sendMail(email,'BX-MSHOTP',msg)
+         await newUser.save()
+         return resp.status(200).json({
+             message:newUser
+         })
+     }
+     
+         
+   
 }
 
 const OtpCheck= async(req,resp)=>{
